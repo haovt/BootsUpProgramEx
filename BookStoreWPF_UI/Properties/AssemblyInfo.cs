1db8737d4760dbdd7e6b74622a78c4adba0396f9
Trần Văn Hảo <haovtit@gmail.com>
2021-09-20 08:01:43 +0700

    Add logger

--- BookStoreWPF_UI/Properties/AssemblyInfo.cs
+++ BookStoreWPF_UI/Properties/AssemblyInfo.cs
@@ -15,7 +15,6 @@ using System.Windows;
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -53,3 +52,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
