711446cf7afdf99ac76ab912ffcf4e8e812d44b7
Trần Văn Hảo <haovtit@gmail.com>
2021-09-20 08:00:38 +0700

    Add logger

--- BookStoreWPF_UI/MainWindow.xaml.cs
+++ BookStoreWPF_UI/MainWindow.xaml.cs
@@ -1,9 +1,15 @@
 ﻿using BookStoreService;
 using BookStoreService.Dto;
+using log4net;
+using log4net.Config;
+using Microsoft.Win32.SafeHandles;
 using Ninject;
 using System;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -15,15 +21,36 @@ namespace BookStoreWPF_UI
     /// </summary>
     public partial class MainWindow : Window
     {
-        private static IBSService _service;
-        private FileMode fileMode => (bool)TextMode.IsChecked ? FileMode.Text : FileMode.Json;
+        [DllImport("kernel32.dll", EntryPoint = "GetStdHandle", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        private static extern IntPtr GetStdHandle(int nStdHandle);
+        [DllImport("kernel32.dll", EntryPoint = "AllocConsole", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        private static extern int AllocConsole();
+        private const int STD_OUTPUT_HANDLE = -11;
+        private const int MY_CODE_PAGE = 437;
 
+        private static IBSRepository _service;
+        private BookStoreService.ReadFileMode fileMode => (bool)TextMode.IsChecked ? BookStoreService.ReadFileMode.Text : BookStoreService.ReadFileMode.Json;
+        private IKernel _kernel;
+        private readonly ILog log = LogManager.GetLogger(typeof(MainWindow));
         public MainWindow()
         {
-            InitializeComponent();
+            MDC.Set("machine", Environment.MachineName);
+            AllocConsole();
+            IntPtr stdHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+            SafeFileHandle safeFileHandle = new SafeFileHandle(stdHandle, true);
+            FileStream fileStream = new FileStream(safeFileHandle, FileAccess.Write);
+            Encoding encoding = Encoding.GetEncoding(MY_CODE_PAGE);
+            StreamWriter standardOutput = new StreamWriter(fileStream, encoding) { AutoFlush = true };
+            Console.SetOut(standardOutput);
 
-            IKernel kernel = new StandardKernel(new ServiceModule());
-            _service = kernel.Get<IBSService>();
+            XmlConfigurator.Configure();
+
+            InitializeComponent();
+      
+            _kernel = new StandardKernel(new ServiceModule());
+            _service = fileMode == ReadFileMode.Text ? _kernel.Get<IBSRepository>("TextRepo") 
+                : _kernel.Get<IBSRepository>("JsonRepo");
+
             Refresh();
 
         }
@@ -32,7 +59,8 @@ namespace BookStoreWPF_UI
         {
             if (_service != null)
             {
-                var books = _service.GetAll(fileMode).ToList();
+                log.Info($"GetAll(): get all Books from {fileMode} mode");
+                var books = _service.GetAll().ToList();
                 Books.ItemsSource = books;
             }    
         }
@@ -40,7 +68,8 @@ namespace BookStoreWPF_UI
         private void Delete_OnClick(object sender, RoutedEventArgs e)
         {
             var selectedItem = Books.SelectedItem as BookDto;
-            _service.DeleteBook(selectedItem.Id, fileMode);
+            log.Info($"Delete book with Id={selectedItem.Id}");
+            _service.DeleteBook(selectedItem.Id);
             Refresh();
         }
 
@@ -55,8 +84,9 @@ namespace BookStoreWPF_UI
 
             if (result.HasValue && result.Value)
             {
+                log.Info($"Update book with Id={selectedItem.Id}");
                 // Update list view
-                _service.UpdateBook(dataContext, fileMode);
+                _service.UpdateBook(dataContext);
                 Refresh();
             }
         }
@@ -73,10 +103,12 @@ namespace BookStoreWPF_UI
             {
                 try
                 {
-                    _service.AddBook(newBook, fileMode);
+                    log.Info($"Add new book: Title={newBook.Title}, Author={newBook.Author}, Price={newBook.Price}");
+                    _service.AddBook(newBook);
                 }
                 catch (Exception ex)
                 {
+                    log.Error(ex.Message);
                     MessageBox.Show(ex.Message);
                 }
                
@@ -92,6 +124,13 @@ namespace BookStoreWPF_UI
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
+            if (_kernel == null)
+            {
+                _kernel = new StandardKernel(new ServiceModule());
+            }
+
+            _service = fileMode == ReadFileMode.Text ? _kernel.Get<IBSRepository>("TextRepo")
+                : _kernel.Get<IBSRepository>("JsonRepo");
             Refresh();
         }
     }
